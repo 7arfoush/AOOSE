3ef779a70c864894b2c448fb82f86336893fa04d
7arfoush <7arfoush@outlook.com>
2018-12-08 19:26:11 +0200

    Modifications in CreateAccount

--- Tickt/src/GUI/CreateAccount.java
+++ Tickt/src/GUI/CreateAccount.java
@@ -122,6 +122,11 @@ public class CreateAccount extends javax.swing.JFrame {
         jLabel3.setText("Have an account?");
 
         jButton1.setText("Click Here to Log in");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -165,6 +170,12 @@ public class CreateAccount extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        new Login().setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
